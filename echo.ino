afc6916c45804835d243d87ea0b27ade2a23fea1
benjaminemanuel13 <benjaminemanuel13@gmail.com>
2022-07-26 11:58:41 +0100

    Add files via upload
    Update

--- echo.ino
+++ echo.ino
@@ -1,7 +1,14 @@
 #include <HardwareSerial.h>
 
 int count = 1;
-int av = 0;
+int chars = 0;
+
+//bool first = true;
+bool finished = false;
+bool done = false;
+uint8_t buff[150];
+
+//int av = 0;
 HardwareSerial MySerial(1);
 
 char charset[256];
@@ -85,26 +92,23 @@ void loadChars()
 void setup() {
     loadChars();
     
-    MySerial.setRxBufferSize(2);
-    MySerial.setTxBufferSize(2);
-    MySerial.begin(9600, SERIAL_8N1, 4, 5);
+    MySerial.setRxBufferSize(1);
+    MySerial.setTxBufferSize(1);
+    MySerial.begin(600, SERIAL_8N1, 4, 5);
     Serial.begin(115200);
     MySerial.setPins(4, 5, 16, 17);
     MySerial.setHwFlowCtrlMode(HW_FLOWCTRL_CTS_RTS, 1);
 }
-bool first = true;
-bool finished = false;
-bool done = false;
-uint8_t buff[150];
 
 char* getCommand()
 {
-  char comm[count - 1];
-
-  for(int i = 0; i < count - 1; i++)
+  char comm[chars];
+  int thi = 0;
+  
+  for(int i = 1; i < chars - 1; i++)
   {
       uint8_t val = buff[i];
-      comm[i] = charset[val];
+      comm[thi++] = charset[val];
   }
 
   return comm;
@@ -134,19 +138,48 @@ void loop() {
 
     if(finished && !done)
     {
+      while(MySerial.available() >= 1)
+      {
+        MySerial.read();
+        Serial.println("Clearing...");
+      }
+      
+      int cou = 1;
       //At this point we have the command
-      char* command = getCommand();
+      //char* command = getCommand();
 
       //do something based on command...
-
+      
+      delay(150);
       //currently echoes
-      for(int i = 0; i < count - 1; i++)
+      Serial.println("OUTGOING");
+      for(int i = 0; i < chars; i++)
       {
-        MySerial.write(buff[i]);
-        delay(100);
+        if(buff[i] >= 1)
+        {
+          Serial.print(i);
+          Serial.print(":");
+          Serial.println(buff[i]);
+          MySerial.write(buff[i]);
+          MySerial.flush();
+          delay(150);
+        }
       }
 
-      done = true;
+      MySerial.write(6);
+      MySerial.write(4);
+      
+
+      while(MySerial.available() >= 1)
+      {
+        MySerial.read();
+        Serial.println("Clearing2...");
+      }
+      
+      done = false;
+      finished = false;
+      count = 1;
+      chars = 0;
     }
   }
 }
@@ -155,10 +188,10 @@ void doCommandRead()
 {
   uint8_t byteFromSerial = 0;
   
-  while(MySerial.available() !=0 && byteFromSerial != 185)
+  while(MySerial.available() >= 1 && count < 60 && !finished)
   {  
     byteFromSerial = MySerial.read();
-    delay(10);
+    delay(50);
     
     if(byteFromSerial > 0)
     {
@@ -166,16 +199,21 @@ void doCommandRead()
       Serial.print(":");
       Serial.println(byteFromSerial);
 
-      if(count < 150)
+      if(count < 60)
       {
         buff[count] = byteFromSerial;
       }
 
-      if(byteFromSerial == 185)
+      if(byteFromSerial == 185 || count > 58)
       {
         finished = true;
       }
 
+      if(byteFromSerial == 133 && chars == 0)
+      {
+        chars = count + 1;
+      }
+      
       count++;
     }
   }  
